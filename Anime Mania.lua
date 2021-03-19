69294f5e70bcb3e1ccc12f4d570f188d42c10d05
Terebi <69407980+HDTerebi@users.noreply.github.com>
2021-03-18 20:52:59 -0400

    Update Anime Mania.lua

--- Anime Mania.lua
+++ Anime Mania.lua
@@ -47,9 +47,9 @@ for _,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
     
 for i,v in pairs(game.Workspace.Living:GetChildren()) do
         if v:FindFirstChild("AI") then
-            if v.Humanoid.Health > 1 then
+            if v.Humanoid.Health > 0 then
                 repeat wait()
-                    game.workspace.Living[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = v:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0, 0, 5)
+                    game.workspace.Living[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame * CFrame.new(0, 0, 5)
                     game:GetService("Players").LocalPlayer.Backpack.Input.Remote:FireServer({ "Light" })
 		    game:GetService("Players").LocalPlayer.Backpack.Input.Remote:FireServer({ "Skill", "1" })
                     game:GetService("Players").LocalPlayer.Backpack.Input.Remote:FireServer({ "Light" })
