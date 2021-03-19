57c5b2964daaa19c0e352bcc29f2cdf04b766ec0
Terebi <69407980+HDTerebi@users.noreply.github.com>
2021-03-18 21:37:55 -0400

    Update Anime Mania.lua

--- Anime Mania.lua
+++ Anime Mania.lua
@@ -29,6 +29,7 @@ end
 
 while wait() do
     pcall(function()
+        wait()
     for _,v in pairs(game.Workspace.Living[game.Players.LocalPlayer.Name]:GetDescendants()) do
 		if v.Name == "Ban" then
 		    game.Workspace.Living[game.Players.LocalPlayer.Name].Ban:Destroy()
@@ -43,12 +44,11 @@ for _,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
 		if v.Name == "ArenaClient" then
 		    game:GetService("Players").LocalPlayer.Backpack.ArenaClient.Disabled = true
 		end
-    end
-    
+end
+
 for i,v in pairs(game.Workspace.Living:GetChildren()) do
         if v:FindFirstChild("AI") then
             if v.Humanoid.Health > 0 then
-                repeat wait()
                     game.workspace.Living[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame * CFrame.new(0, 0, 5)
                     game:GetService("Players").LocalPlayer.Backpack.Input.Remote:FireServer({ "Light" })
 		    game:GetService("Players").LocalPlayer.Backpack.Input.Remote:FireServer({ "Skill", "1" })
@@ -58,7 +58,6 @@ for i,v in pairs(game.Workspace.Living:GetChildren()) do
                     game:GetService("Players").LocalPlayer.Backpack.Input.Remote:FireServer({ "Skill", "3" })
                     game:GetService("Players").LocalPlayer.Backpack.Input.Remote:FireServer({ "Light" })
                     game:GetService("Players").LocalPlayer.Backpack.Input.Remote:FireServer({ "Skill", "4" })
-until v.Humanoid.Health <= 0
         end
             end
 end
